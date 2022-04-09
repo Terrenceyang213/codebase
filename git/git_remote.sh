5d95b302a75e952e85ff9c4d14c69d65fbd872d4
Terry <terrence-yang@foxmail.com>
2022-04-09 10:28:08 +0800

    add git_remote.sh

---
+++ git/git_remote.sh
@@ -0,0 +1,6 @@
+# 克隆会把所有得版本拷贝下来。
+$ git clone https://github.com/libgit2/libgit2
+## 拷贝加更名
+$ git clone https://github.com/libgit2/libgit2 mylibgit
+## SSH传输协议
+$ git clone user@server:path/to/repo.git 
